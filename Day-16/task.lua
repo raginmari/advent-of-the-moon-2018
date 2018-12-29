dfd5b8ac1c09193296ca2a74c2fcdb13cd6846d7
Reimar Twelker <twelker@adesso-mobile.de>
2018-12-29 22:48:27 +0100

    day-16: Solved day 16, part 1.

--- Day-16/task.lua
+++ Day-16/task.lua
@@ -7,7 +7,7 @@ function registers(a, b, c, d)
 end
 
 function instruction(num, in1, in2, out)
-	return { number = num, inputs = { in1, in2 }, output = out }
+	return { number = num, inputA = in1, inputAr = in1 + 1, inputB = in2, inputBr = in2 + 1, output = out + 1 }
 end
 
 function sample(instruction, before, after)
@@ -49,6 +49,103 @@ function parse_samples(lines)
 			state = 0
 		end
 	end
+
+	return samples
+end
+
+function addr(registers, instruction)
+	registers[instruction.output] = registers[instruction.inputAr] + registers[instruction.inputBr]
+end
+
+function addi(registers, instruction)
+	registers[instruction.output] = registers[instruction.inputAr] + instruction.inputB
+end
+
+function mulr(registers, instruction)
+	registers[instruction.output] = registers[instruction.inputAr] * registers[instruction.inputBr]
+end
+
+function muli(registers, instruction)
+	registers[instruction.output] = registers[instruction.inputAr] * instruction.inputB
+end
+
+function banr(registers, instruction)
+	registers[instruction.output] = bit32.band(registers[instruction.inputAr], registers[instruction.inputBr])
+end
+
+function bani(registers, instruction)
+	registers[instruction.output] = bit32.band(registers[instruction.inputAr], instruction.inputB)
+end
+
+function borr(registers, instruction)
+	registers[instruction.output] = bit32.bor(registers[instruction.inputAr], registers[instruction.inputBr])
+end
+
+function bori(registers, instruction)
+	registers[instruction.output] = bit32.bor(registers[instruction.inputAr], instruction.inputB)
+end
+
+function setr(registers, instruction)
+	registers[instruction.output] = registers[instruction.inputAr]
+end
+
+function seti(registers, instruction)
+	registers[instruction.output] = instruction.inputA
+end
+
+function gtir(registers, instruction)
+	if instruction.inputA > registers[instruction.inputBr] then
+		registers[instruction.output] = 1
+	else 
+		registers[instruction.output] = 0
+	end
+end
+
+function gtri(registers, instruction)
+	if registers[instruction.inputAr] > instruction.inputB then
+		registers[instruction.output] = 1
+	else 
+		registers[instruction.output] = 0
+	end
+end
+
+function gtrr(registers, instruction)
+	if registers[instruction.inputAr] > registers[instruction.inputBr] then
+		registers[instruction.output] = 1
+	else 
+		registers[instruction.output] = 0
+	end
+end
+
+function eqir(registers, instruction)
+	if instruction.inputA == registers[instruction.inputBr] then
+		registers[instruction.output] = 1
+	else 
+		registers[instruction.output] = 0
+	end
+end
+
+function eqri(registers, instruction)
+	if registers[instruction.inputAr] == instruction.inputB then
+		registers[instruction.output] = 1
+	else 
+		registers[instruction.output] = 0
+	end
+end
+
+function eqrr(registers, instruction)
+	if registers[instruction.inputAr] == registers[instruction.inputBr] then
+		registers[instruction.output] = 1
+	else 
+		registers[instruction.output] = 0
+	end
+end
+
+function is_equal(registers1, registers2)
+	for i = 1, #registers1 do
+		if registers1[i] ~= registers2[i] then return false end
+	end
+	return true
 end
 
 function solve(input)
@@ -57,13 +154,29 @@ function solve(input)
 
 	local lines = utils.array_filter(input, function(a) return a:len() > 0 end)
 	local samples = parse_samples(lines)
+	print(string.format("Parsed %d samples", #samples))
 
+	local functions = { addr, addi, mulr, muli, banr, bani, borr, bori, setr, seti, gtir, gtri, gtrr, eqir, eqri, eqrr }
+	local registers = { 0, 0, 0, 0 }
+	local result = 0
+	for _, sample in ipairs(samples) do
+		local n = 0
+		for _, f in ipairs(functions) do
+			utils.array_copy(sample.before, registers)
+			f(registers, sample.instruction)
+			if is_equal(registers, sample.after) then n = n + 1 end
+		end
+		if n >= 3 then 
+			result = result + 1 
+		end
+	end
+	
 	local time = os.clock() - t0
 	print(string.format("Elapsed time: %.4f", time))
 
 	return result
 end
 
-local filename = arg[1] or "input.txt"
+local filename = arg[1] or "input-samples.txt"
 local input = input.read_file(filename)
 print("Solution: " .. (solve(input) or "not found"))
