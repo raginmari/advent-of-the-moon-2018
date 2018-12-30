b3ad7d7446f1392a41354b5b8f17d0eebea32dfc
Reimar Twelker <twelker@adesso-mobile.de>
2018-12-30 12:47:29 +0100

    day-16: Solved day 16, part 2.

--- Day-16/task2.lua
+++ Day-16/task2.lua
@@ -166,6 +166,79 @@ function is_equal(registers1, registers2)
 	return true
 end
 
+function intersection(a, b)
+	local t = {}
+	local i = 1
+	local j = 1
+	while i <= #a and j <= #b do
+		if a[i] < b[j] then 
+			i = i + 1 
+		elseif b[j] < a[i] then
+			j = j + 1
+		else
+			t[#t + 1] = a[i]
+			i = i + 1
+		end
+	end
+	return t
+end
+
+function find_candidates(samples, functions)
+	local registers = { 0, 0, 0, 0 }
+	local candidates = {}
+	
+	for _, sample in ipairs(samples) do
+		local function_indexes = {}
+		for i, f in ipairs(functions) do
+			utils.array_copy(sample.before, registers)
+			f(registers, sample.instruction)
+			if is_equal(registers, sample.after) then 
+				function_indexes[#function_indexes + 1] = i
+			end
+		end
+
+		local opcode = sample.instruction.number
+		if #function_indexes == 1 or not candidates[opcode] then
+			candidates[opcode] = utils.array_copy(function_indexes)
+		else
+			candidates[opcode] = intersection(candidates[opcode], function_indexes)
+		end
+	end
+
+	return candidates
+end
+
+function resolve_opcodes(samples, functions)
+	
+	local candidates = find_candidates(samples, functions)
+
+	local unresolved = {}
+	for opcode, candidates in pairs(candidates) do
+		unresolved[#unresolved + 1] = { opcode = opcode, candidates = candidates }
+	end
+
+	local opcodes = {}
+	while #unresolved > 0 do
+		-- Sort by number of candidates in descending order
+		table.sort(unresolved, function(a, b) return #a.candidates > #b.candidates end)
+		
+		-- Take the opcode with the fewest candidates
+		local nxt = unresolved[#unresolved] 
+		unresolved[#unresolved] = nil
+
+		if #nxt.candidates > 1 then os.exit() end -- There must always be one with only 1 candidate
+
+		local function_index = nxt.candidates[1]
+		opcodes[nxt.opcode] = functions[function_index]
+
+		for _, t in ipairs(unresolved) do
+			utils.array_remove_where(t.candidates, function(a) return a == function_index end)
+		end
+	end
+
+	return opcodes
+end
+
 function solve(input)
 
 	local t0 = os.clock()
@@ -175,10 +248,16 @@ function solve(input)
 	print(string.format("Parsed %d program instructions", #program))
 
 	local functions = { addr, addi, mulr, muli, banr, bani, borr, bori, setr, seti, gtir, gtri, gtrr, eqir, eqri, eqrr }
-	local registers = { 0, 0, 0, 0 }
+	local opcodes = resolve_opcodes(samples, functions)
 
-	
-	
+	local registers = { 0, 0, 0, 0 }
+	for _, instruction in ipairs(program) do
+		local f = opcodes[instruction.number]
+		f(registers, instruction)
+	end
+
+	local result = registers[1]
+
 	local time = os.clock() - t0
 	print(string.format("Elapsed time: %.4f", time))
 
